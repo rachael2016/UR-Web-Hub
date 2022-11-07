39158ff92e5c02f68e18dfd614dcab1098da0830
Emersyn Harris <eharr21@u.rochester.edu>
2022-11-07 17:12:06 -0500

    buildings csv to database

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Kelly Thornton
 Grant Yap
 
 Rachael He 
+
+#Startup
+
+Create the virtual environment, activate it, and install all required dependencies. If you do not know how to do this, follow in instructions at https://flask.palletsprojects.com/en/2.2.x/installation/
+
+To create the databases, run init__db.py
+
+After you've done this, the Flask app is ready to be served.
--- init__db.py
+++ init__db.py
@@ -1,4 +1,4 @@
-import sqlite3, csv
+import sqlite3, csv, csvfiles
 import os
 
 connection = sqlite3.connect('databases.db')
@@ -7,6 +7,12 @@ with open('schema.sql') as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
+
+with open(r'csvfiles\buildings.csv') as fin:
+    dr = csv.reader(fin)
+    dr = list(dr)
+    for i in dr:
+        cur.execute("INSERT INTO Buildings(name) VALUES (?);", i)
 
 print("complete")
 connection.commit()
