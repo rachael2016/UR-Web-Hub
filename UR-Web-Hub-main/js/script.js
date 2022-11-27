04d453e36bf7750b1011be03cb9333b5b3961b98
Nafix <SarahX@users.noreply.github.com>
2022-11-27 15:14:57 -0500

    oops accidentally uploaded a folder

--- UR-Web-Hub-main/js/script.js
+++
@@ -1,8 +0,0 @@
-function openAccordion(id) {
-  var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else { 
-    x.className = x.className.replace(" w3-show", "");
-  }
-}
