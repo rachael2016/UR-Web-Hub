801027a3467cfbabdd36a4ed868640e5761f050a
Nafix <SarahX@users.noreply.github.com>
2022-11-28 15:10:46 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,3 +6,12 @@ function openAccordion(id) {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+
+function openResponsiveMenu() {
+  var x = document.getElementById("navMenu");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+} 
