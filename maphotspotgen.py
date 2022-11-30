de56808736f24a787b80db4e003c9b5cc7f11df0
Emersyn Harris <eharr21@u.rochester.edu>
2022-11-30 18:31:40 -0500

    map points hotspotted

--- maphotspotgen.py
+++ maphotspotgen.py
@@ -6,12 +6,18 @@ conn = getdbconnection()
 buildings = conn.execute('SELECT * FROM Buildings').fetchall()
 
 buildingsData = []
+count = 1
 for building in buildings:
-    buildingsData.append({'name': building['name'], 'xcoord' : building['xcoord'], 'ycoord' : ['ycoord']})
+    buildingsData.append({'name': building['name'], 'xcoord' : building['xcoord'], 'ycoord' : building['ycoord'], 'id' : count})
+    count += 1
     
 conn.close()
 
-with open('maphotspotautogen.txt') as f:
+with open('maphotspotautogen.txt', 'w') as f:
     for building in buildingsData:
         #here will go the href and coordinate info
-        f.write('')
+        aherf = '<area target="" alt="" title="" href="downdetector/' + str(building.get("id")) + '"coords="'+ str(building.get("xcoord")) + ','+ str(building.get("ycoord")) + ',8" shape="circle">\n'
+        print(aherf)
+        f.write(aherf)
+
+f.close()
