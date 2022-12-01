cb96489c7a4f9f185135a5c119b7bb0c5320ab15
Nafix <SarahX@users.noreply.github.com>
2022-11-30 23:11:11 -0500

    deleting function
    switching to another approach

--- js/script.js
+++ js/script.js
@@ -6,12 +6,3 @@ function openAccordion(id) {
     x.className = x.className.replace(" w3-show", "");
   }
 }
-
-function openResponsiveMenu() {
-  var x = document.getElementById("navMenu");
-  if (x.className === "menu") {
-    x.className += " responsive";
-  } else {
-    x.className = "menu";
-  }
-} 
