6dfd254ab8f4766f5a3c04617f5d174ed56272da
Emersyn Harris <eharr21@u.rochester.edu>
2022-12-02 14:48:19 -0500

    feedback form base created

--- forms.py
+++ forms.py
@@ -1,7 +1,14 @@
-from wtforms import BooleanField, SubmitField, ValidationError
+from wtforms import BooleanField, SubmitField, ValidationError, TextAreaField, EmailField, StringField
 from flask_wtf import FlaskForm
 from wtforms.validators import EqualTo, DataRequired, Length
 
 class UserReport(FlaskForm):
     status = BooleanField("Down Status", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class FeedbackForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    subject = StringField("Subject", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
     submit = SubmitField("Submit")
